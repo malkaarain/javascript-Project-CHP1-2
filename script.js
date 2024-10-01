4ae13316bff0cc8ac1fd534ae9d3e8d8a900ec75
Malka arain <malkaarain92@gmail.com>
2024-10-02 03:08:20 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,91 @@
+//CHPTER 1.Alert Chapter
+
+ //Q:1
+
+// alert(message);
+
+ //Q:2
+// alert("Welcome to my website!");
+
+ //Q:3
+//   alert("Welcome to my website!" . toUpperCase());
+
+
+ //Q:4
+// alert("Hello, world!");
+
+
+//Q:5
+//alert("Hello, world!");
+
+
+//CHPTER 2.Variables for Strings
+
+
+ //Q:1
+//  var message = "Hello, world!";
+// alert(message);
+
+
+//Q:2
+// var age;
+// age = 25;
+
+// alert(age);
+
+
+//Q:3
+// var greeting = "Good morning!";
+// alert(greeting);
+
+// greeting = "Good evening!";
+// alert(greeting);
+
+
+//Q:4
+// var age;
+// age = 25;
+
+// alert(age);
+
+
+//Q:5
+// var favoriteColor = "blue";
+// alert("My favorite color is " + favoriteColor);
+
+
+//Q:6
+// var age;
+// age = 25; 
+// alert("The value of age is " + age);
+
+
+
+//CHPTER 3.Variables for Numbers
+
+
+ //Q:1
+// var height = 170; 
+// var newHeight = height + 10;
+// alert("The new height is " + newHeight); 
+
+
+//Q:2
+// var num1 = 50;
+// var num2 = 30; 
+// var sum = num1 + num2; 
+// alert("The sum is " + sum);
+
+
+//Q:3
+// var numToBeAdded = 20; 
+// var total = numToBeAdded + 10; 
+// alert("The total is " + total);
+
+
+//Q:4
+
+// var price = 120; 
+// var tax = 0.10; 
+// var totalPrice = price + (price * tax); 
+// alert("The total price is " + totalPrice);
